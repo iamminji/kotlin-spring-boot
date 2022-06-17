ba781579145db680fe169951b7fadc17c62d664b
iamminji <minji.dev@gmail.com>
2022-06-17 23:26:53 +0900

    update gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 val developmentOnly by configurations.creating
 configurations {
